f1be516343a9f4ae6cab282b68a9417b695cb158
tropik555 <yevhen.levchenko@nure.ua>
2021-09-06 21:52:03 +0300

    updated sql files

--- patient-service/src/main/resources/db.changelog/v-1.0/01-drop-patients-table.sql
+++ patient-service/src/main/resources/db.changelog/v-1.0/01-drop-patients-table.sql
@@ -1,3 +1,13 @@
+ALTER TABLE appointment
+DROP CONSTRAINT  FK4apif2ewfyf14077ichee8g06;
+
+ALTER TABLE appointment
+DROP CONSTRAINT  FKr3dgxvrvoonmuaopg4w8ifugs;
+
+DROP INDEX UK_hceh1f14irmc0x37f7bfulmo2;
+
+
+
 drop table appointment;
 drop table shift;
 drop table patient;
