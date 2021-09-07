b96f2027d9194d894c0b641fe41b63052684239f
san <vencer97shoes@gmail.com>
2021-09-07 09:38:49 +0200

    added to ribbon of Telegram,list of procedures

--- telegram-bot/src/main/java/com/example/alexthbot/fab/services/ProcedureServiceApi.java
+++ telegram-bot/src/main/java/com/example/alexthbot/fab/services/ProcedureServiceApi.java
@@ -4,24 +4,37 @@ package com.example.alexthbot.fab.services;
 import com.example.alexthbot.fab.utils.CollectionParams;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+
 @Slf4j
 @Service
-@ConditionalOnProperty(prefix = "gateway", name = "state", havingValue = "true")
 public class ProcedureServiceApi implements ProcedureService {
 
-    @Value("${gateway.api.get-all-doctors}")
-    private String urlGetProcedure;
+    @Value("http://192.168.0.118:8762/patient/doctor/1/shifts")
+    private String urlGetProcedureOfTooth;
+    @Value("http://192.168.0.118:8762/patient/doctor/2/shifts")
+    private String urlGetProcedureOfFrags;
 
     @Override
-    public List<Procedure> getProcedures() {
+    public List<Procedure> getProceduresOfTooth() {
         HttpEntity<Void> httpEntity = new HttpEntity<>(new HttpHeaders());
-        ResponseEntity<List<Procedure>> proceduresEntity = new RestTemplate().exchange(urlGetProcedure, HttpMethod.GET, httpEntity, CollectionParams.get());
+        ResponseEntity<List<Procedure>> proceduresEntity = new RestTemplate().exchange(urlGetProcedureOfTooth, HttpMethod.GET, httpEntity, CollectionParams.get());
+        if (proceduresEntity.getStatusCode() == HttpStatus.OK) {
+            log.info(proceduresEntity.getBody().toString());
+            return proceduresEntity.getBody();
+        } else {
+            throw new RuntimeException();//создадим свой кастомный
+        }
+    }
+
+    @Override
+    public List<Procedure> getProceduresOfDrags() {
+        HttpEntity<Void> httpEntity = new HttpEntity<>(new HttpHeaders());
+        ResponseEntity<List<Procedure>> proceduresEntity = new RestTemplate().exchange(urlGetProcedureOfFrags, HttpMethod.GET, httpEntity, CollectionParams.get());
         if (proceduresEntity.getStatusCode() == HttpStatus.OK) {
             log.info(proceduresEntity.getBody().toString());
             return proceduresEntity.getBody();
